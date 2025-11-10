5b892c117a28836d0611c3a4adea3991f58076b4
1cedrus <aowiej23s@gmail.com>
2025-11-10 16:27:06 +0700

    Fix lack of export

--- packages/chain-specs/src/index.ts
+++ packages/chain-specs/src/index.ts
@@ -6,14 +6,18 @@ export { chainSpec as paseo } from './specs/paseo.js';
 export { chainSpec as polkadot_asset_hub } from './specs/polkadot_asset_hub.js';
 export { chainSpec as ksmcc3_asset_hub } from './specs/ksmcc3_asset_hub.js';
 export { chainSpec as westend2_asset_hub } from './specs/westend2_asset_hub.js';
+export { chainSpec as paseo_asset_hub } from './specs/paseo.js';
 
 export { chainSpec as polkadot_bridge_hub } from './specs/polkadot_bridge_hub.js';
 export { chainSpec as ksmcc3_bridge_hub } from './specs/ksmcc3_bridge_hub.js';
 export { chainSpec as westend2_bridge_hub } from './specs/westend2_bridge_hub.js';
+export { chainSpec as paseo_bridge_hub } from './specs/westend2_bridge_hub.js';
 
 export { chainSpec as polkadot_collectives } from './specs/polkadot_collectives.js';
 export { chainSpec as westend2_collectives } from './specs/westend2_collectives.js';
+export { chainSpec as paseo_collectives } from './specs/westend2_collectives.js';
 
 export { chainSpec as ksmcc3_people } from './specs/ksmcc3_people.js';
 export { chainSpec as westend2_people } from './specs/westend2_people.js';
 export { chainSpec as polkadot_people } from './specs/polkadot_people.js';
+export { chainSpec as paseo_people } from './specs/polkadot_people.js';
